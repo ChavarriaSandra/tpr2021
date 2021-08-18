601957b7f98744635bc4b2d25294d318716ce4e5
Chavarria <sandrachavarria729@gmail.com>
2021-08-17 20:05:39 -0400

    integracion de blade

---
+++ resources/views/cursos/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.plantilla')
+
+@section('title','cursos')
+
+@section('content')
+    <h1>Bienvenido a la pagina principal del curso</h1>
+@endsection
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.plantilla')
+@section('title','Home')
+
+@section('content')
+    <h1>Bienvenido a la Paniga Principal</h1>
+@endsection
